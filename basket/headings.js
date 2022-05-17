f6e8f348dafd3761c03e4320f83dc9f6c4edc588
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-05-17 21:49:20 +0630

    frontend to decide filterer component name

--- basket/headings.js
+++ basket/headings.js
@@ -1,7 +1,7 @@
 function getHeadingFilters(){
     const filters=[];
     const lbl=this.getHeadingLabel();
-    if (lbl.hasname) filters.push({name:lbl.name,filter:"entry_"+ this.langOf()}); //chunk as entry
+    if (lbl.hasname) filters.push({name:lbl.name}); //chunk as entry
     for (let attrname in lbl.attrdef) {
         filters.push({...lbl.attrdef[attrname],attrname});
     }
